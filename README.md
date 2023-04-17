1913da37ca301d3e5ea2dd3d4b45202d8e079eba
1ups_ <61560124+1upses@users.noreply.github.com>
2023-04-17 18:48:18 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Brainfuck Interpreter in Python
+
+This is a simple interpreter for the Brainfuck programming language, written in Python. The interpreter takes a file containing Brainfuck code as input and executes it.
+
+## Requirements
+
+To run the interpreter, you need to have Python installed on your computer. You can download Python from the official website [here](https://www.python.org/downloads/).
+
+## Usage
+
+To use the interpreter, simply run the following command:
+
+```
+python bf.py <filename>
+```
+
+
+Replace `<filename>` with the name of the file containing the Brainfuck code that you want to execute.
+
+For example, if you have a file called `hello.bf` containing the following code:
+
+```
+>++++++++[<+++++++++>-]<.>++++[<+++++++>-]<+.+++++++..+++.>>++++++[<+++++++>-]<++.------------.>++++++[<+++++++++>-]<+.<.+++.------.--------.>>>++++[<++++++++>-]<+.
+```
+
+You can run the interpreter with the following command:
+
+```
+python bf.py hello.bf
+```
+
+and the output should be `Hello, World!`
---
+++ bf.py
@@ -0,0 +1,51 @@
+from linked_lists import linked_list
+import sys, msvcrt
+
+def open_file(filename):
+    temp = ""
+    string = ""
+    with open(filename, "r") as f:
+        temp = f.read()
+    for char in temp:
+        if char in "><+-.,[]":
+            string += char
+    if string.count("[") != string.count("]"):
+        print("Error: unmatched brackets")
+        sys.exit(1)
+    return string
+
+if __name__ == "__main__":
+    
+    if len(sys.argv) != 2:
+        print("Usage: python bf.py <filename>")
+        sys.exit(1)
+
+    string = open_file(sys.argv[1])
+    pointer = i = 0
+    array = linked_list()
+
+    while i < len(string):
+        match string[i]:
+            case ">":
+                pointer += 1
+            case "<":
+                pointer -= 1
+            case "+":
+                array.increment(pointer)
+            case "-":
+                array.decrement(pointer)
+            case ".":
+                print(chr(array.get(pointer)), end="")
+            case ",":
+                array.set(pointer, ord(msvcrt.getch()))
+            case "[":
+                if array.get(pointer) == 0:
+                    while string[i] != "]":
+                        i += 1
+            case "]":
+                if array.get(pointer) != 0:
+                    while string[i] != "[":
+                        i -= 1
+        i += 1
+
+    sys.exit(0)
